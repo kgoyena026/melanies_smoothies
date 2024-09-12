23ad85dd89a38edd7d8236f750351e4a56d5909f
kgoyena026 <goyenakg@gmail.com>
2024-09-12 12:36:08 +0800

    Update streamlit_app.py
    Edited positioning of requests lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -28,6 +29,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" +ingredients_string+"""','"""+name_on_order+ """')"""
@@ -41,8 +44,4 @@ if ingredients_list:
        # confirmation = "Your smoothie is ordered, ",name_on_order,"!"
         st.success(f"Your smoothie is ordered, {name_on_order}!",icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
